408903df350f21b0dcc7586d4d60f82cbcf5cc2d
Luke Mackenzie <luke@lukem.co.uk>
2025-10-21 16:23:04 +0100

    added route stubs

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.json({message: 'get users'})
+})
+
+router.get('/:id', (req, res) => {
+    res.json({message: 'get user'})
+})
+
+router.put('/:id', (req, res) => {
+    res.json({message: 'user updated'})
+})
+
+router.delete('/:id', (req, res) => {
+    res.json({message: 'deleted user'})
+})
+
+export default router
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import express from 'express'
+import authRoutes from './routes/authRoutes.ts'
+import userRoutes from './routes/userRoutes.ts'
+import habitRoutes from './routes/habitRoutes.ts'
+
 
 const app = express()
 
@@ -6,11 +10,9 @@ app.get('/health', (req, res) => {
     res.json({ message: 'hello'}).status(200)
 })
 
-app.post('/cake',(req,res) => {
-    res.send('ok')
-})
-
-
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/habits', habitRoutes)
 
 export { app }
 
