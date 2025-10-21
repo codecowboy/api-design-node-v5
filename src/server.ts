cce1c03aff8cbc27a9655fd0b4dfbf4eeecdcd18
Luke Mackenzie <luke@lukem.co.uk>
2025-10-21 15:09:21 +0100

    most recent changes from cosmic

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,12 @@ app.get('/health', (req, res) => {
     res.json({ message: 'hello'}).status(200)
 })
 
+app.post('/cake',(req,res) => {
+    res.send('ok')
+})
+
+
+
 export { app }
 
 export default app
