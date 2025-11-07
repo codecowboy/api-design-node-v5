30293c65b32091260365c082a260b15cfcc6cb85
Luke Mackenzie <luke@lukem.co.uk>
2025-11-07 14:28:12 +0000

    Added password hashing and login route

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,10 @@
-import type {Request, Response} from 'express'
+import type { Request, Response} from 'express'
 import bcrypt from 'bcrypt'
-import {db} from '../db/connection.ts'
+import { db } from '../db/connection.ts'
 import { users, type NewUser } from '../db/schema.ts'
 import { generateToken } from '../utils/jwt.ts'
-import { hashPassword } from '../utils/passwords.ts'
+import { comparePasswords, hashPassword } from '../utils/passwords.ts'
+import { eq } from 'drizzle-orm'
 
 export const register = async (req:Request<any, any, NewUser>, res:Response) => {
     try {
@@ -39,4 +40,45 @@ export const register = async (req:Request<any, any, NewUser>, res:Response) => 
         console.error('Registration error', e)
         res.status(500).json({error: 'Failed to create user'})
     }
-}       
+}
+
+export const login = async (req:Request, res: Response) => {
+        try {
+            const {email, password} = req.body
+            const user = await db.query.users.findFirst({
+                where: eq(users.email, email),
+            })
+            if (!user) {
+                return res.status(401).json({ error: 'Invalid credentials' })
+            }
+            const isValidatedPassword = await comparePasswords(password, user.password)
+
+            if (!isValidatedPassword) {
+                return res.status(401).json({error: 'Invalid credentials'})
+            }
+            const token = await generateToken({
+                id:user.id,
+                email: user.email,
+                username: user.username,
+
+            })
+            return res.json({
+                message: 'login success',
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    username: user.username,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    createdAt: user.createdAt
+                },
+                token,
+
+            }).status(201)
+
+        } catch (e) {
+            console.error('login error',e)
+            res.status(500).json({error: 'Failed to login'})
+            
+        }
+}
--- src/utils/passwords.ts
+++ src/utils/passwords.ts
@@ -5,3 +5,7 @@ export const hashPassword = async (password: string) => {
     return bcrypt.hash(password, env.BCRYPT_ROUNDS)
 
 }
+
+export const comparePasswords = async (password: string, hashedPassword: string) => {
+    return bcrypt.compare(password, hashedPassword)
+}
